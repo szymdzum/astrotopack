bb8982e0a87f8c64ee325e2dfd8b5b91d307f722
Kumak <szymon@kumak.dev>
2024-02-12 01:13:20 +0100

    more stuff done

---
+++ src/data/data.js
@@ -0,0 +1,17 @@
+export const features = [
+  {
+    title: "Opakowania",
+    body: "Opakowania i Produkty Opakowane z Własnym Brandingiem",
+    src: "/images/box.svg",
+  },
+  {
+    title: "Elektronika",
+    body: "Urządzenia Elektryczne i Elektroniczne, Baterie oraz Akumulatory",
+    src: "/images/battery.svg",
+  },
+  {
+    title: "Specjalistyczne",
+    body: "Zużyte Oleje, Opony, Środki Smarowe i Specjalistyczne",
+    src: "/images/barrel.svg",
+  }
+];
