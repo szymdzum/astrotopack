f340256c137f641bc3f7f7731b7abc13e66dc9d7
Kumak <szymon@kumak.dev>
2024-02-12 04:40:21 +0100

    css tweaking

--- src/data/data.js
+++ src/data/data.js
@@ -15,3 +15,26 @@ export const features = [
     src: "/images/barrel.svg",
   }
 ];
+
+export const services = [
+  {
+    title: "Obowiązki prawne",
+    description: "Zidentyfikuję wszelkie wymogi dotyczące opakowań, sprzętu elektronicznego, baterii i akumulatorów, które dotyczą Twojej firmy.",
+    icon: "/images/box.svg",
+  },
+  {
+    title: "Zarządzanie rejestracją",
+    description: "Zajmę się całością procesu rejestracji w bazie danych o produktach i opakowaniach oraz o gospodarce odpadami (BDO).",
+    icon: "/images/box.svg",
+  },
+  {
+    title: "Obsługa sprawozdawcza",
+    description: "Zadbam o coroczną sprawozdawczość związaną z wprowadzaniem produktów na rynek, zapewniając zgodność z wymogami.",
+    icon: "/images/box.svg",
+  },
+  {
+    title: "Optymalizacja kosztów",
+    description: "Przygotuję strategię pozwalającą zminimalizować wydatki przy jednoczesnej pełnej zgodności z obowiązującym prawem.",
+    icon: "/images/box.svg",
+  }
+];
