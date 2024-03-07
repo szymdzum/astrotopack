6864e1a40fc93dc61922e4b8428e4110f016c9a9
Kumak <szymon@kumak.dev>
2024-03-07 03:57:59 +0100

    collection fix

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,13 +3,16 @@ import { z, defineCollection } from 'astro:content';
 const features = defineCollection({
   type: 'data',
   schema: ({ image }) =>
-   z.object({
-    title: z.string(),
-    description: z.string(),
-    imgSrc: image()
-  }),
+  z.array(
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      imgSrc: image()
+    })
+  ),
 });
 
+
 export const collections = {
-features
+  features
 };
