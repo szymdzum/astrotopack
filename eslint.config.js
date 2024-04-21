d9623ddc906ec7ef05f38548889fc023eb0bb7e5
Kumak <szymon@kumak.dev>
2024-04-21 21:53:22 +0200

    Linters config

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,31 @@
 import globals from "globals";
+// import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+//import eslintPluginAstro from "eslint-plugin-astro";
+
+// https://github.com/withastro/astro/blob/237e17c8feea244b06b00ceaed08cb16d3d6baeb/eslint.config.js
+
+// https://github.com/prettier/eslint-config-prettier?tab=readme-ov-file#eslintconfigjs-flat-config-plugin-caveat
+
 
 export default [
+  // js.configs.recommended,
+ // ...eslintPluginAstro.configs['flat/recommended'],
   {
-    globals: {
-      ...globals.browser,
-      ...globals.node,
+  languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node
+        }
     },
-    extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
     rules: {
       semi: "error",
       "no-console": "warn",
       "prefer-const": "error",
+      "no-unused-vars": "error",
+      "no-undef": "error",
     },
   },
+   eslintConfigPrettier,
 ];
+// Thats the eslint.config.js (flat config) 
---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: ["**/*.astro"],
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
