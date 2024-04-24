b11b92215b1031f978a2eb7de50280add5e506ad
Kumak <szymon@kumak.dev>
2024-04-24 19:21:09 +0200

    CSS overhoul

--- src/config/data.js
+++ src/config/data.js
@@ -1,49 +1,49 @@
 export const config = {
-  pageName: "PUT to PACK",
+  pageName: 'Put to Pack',
 };
 
 export const menu = [
   {
-    link: "#features",
-    name: "O Mnie",
+    link: '#features',
+    name: 'O Mnie',
   },
   {
-    link: "#services",
-    name: "Usługi",
+    link: '#services',
+    name: 'Usługi',
   },
   {
-    link: "#faq",
-    name: "FAQ",
+    link: '#faq',
+    name: 'FAQ',
   },
   {
-    link: "#contact",
-    name: "Kontakt",
+    link: '#contact',
+    name: 'Kontakt',
   },
 ];
 
 export const services = [
   {
-    title: "Obowiązki prawne",
+    title: 'Obowiązki prawne',
     description:
-      "Zidentyfikuję wszelkie wymogi dotyczące opakowań, sprzętu elektronicznego, baterii i akumulatorów, które dotyczą Twojej firmy.",
-    icon: "/images/box.svg",
+      'Zidentyfikuję wszelkie wymogi dotyczące opakowań, sprzętu elektronicznego, baterii i akumulatorów, które dotyczą Twojej firmy.',
+    icon: '/images/box.svg',
   },
   {
-    title: "Zarządzanie rejestracją",
+    title: 'Zarządzanie rejestracją',
     description:
-      "Zajmę się całością procesu rejestracji w bazie danych o produktach i opakowaniach oraz o gospodarce odpadami (BDO).",
-    icon: "/images/box.svg",
+      'Zajmę się całością procesu rejestracji w bazie danych o produktach i opakowaniach oraz o gospodarce odpadami (BDO).',
+    icon: '/images/box.svg',
   },
   {
-    title: "Obsługa sprawozdawcza",
+    title: 'Obsługa sprawozdawcza',
     description:
-      "Zadbam o coroczną sprawozdawczość związaną z wprowadzaniem produktów na rynek, zapewniając zgodność z wymogami.",
-    icon: "/images/box.svg",
+      'Zadbam o coroczną sprawozdawczość związaną z wprowadzaniem produktów na rynek, zapewniając zgodność z wymogami.',
+    icon: '/images/box.svg',
   },
   {
-    title: "Optymalizacja kosztów",
+    title: 'Optymalizacja kosztów',
     description:
-      "Przygotuję strategię pozwalającą zminimalizować wydatki przy jednoczesnej pełnej zgodności z obowiązującym prawem.",
-    icon: "/images/box.svg",
+      'Przygotuję strategię pozwalającą zminimalizować wydatki przy jednoczesnej pełnej zgodności z obowiązującym prawem.',
+    icon: '/images/box.svg',
   },
 ];
