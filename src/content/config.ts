708dd8cb5661c43b2ed2679355cc93a4be07cd1f
Kumak <szymon@kumak.dev>
2024-05-01 10:05:46 +0200

    About me addition

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,31 @@
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection } from 'astro:content';
+
+const sectionHeader = z.object({
+  name: z.string(),
+  title: z.string(),
+  subtitle: z.string(),
+});
 
 const features = defineCollection({
-  type: "data",
+  type: 'data',
   schema: ({ image }) =>
     z.array(
       z.object({
         title: z.string(),
         description: z.string(),
         imgSrc: image(),
-      }),
+      })
     ),
+});
+
+export type SectionHeader = z.infer<typeof sectionHeader>;
+
+const sections = defineCollection({
+  type: 'content',
+  schema: sectionHeader,
 });
 
 export const collections = {
   page: features,
+  landing: sections,
 };
---
+++ src/content/landing/hero.md
@@ -0,0 +1,5 @@
+---
+name: 'hero'
+title: 'Put To Pack'
+subtitle: 'Realizuje prawne zobowiązania wprowadzeniowe wynikające z ustaw firmom, które sprzedają produkty w opakowaniach z własnym logo.'
+---
